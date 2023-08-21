439b91e786585bfbc72cb270b2d81fc772c5d3c1
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-08-21 16:13:51 +0530

    User registration api with validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    // res.json([{ name: "Amit", scores: { score: [30, 40, 50] }}])
-    res.json({ name: "Amit", scores: { score: [30, 40, 50] }})
+
+
+//Create a user using: POST "/api/auth/". Doesnt require auth
+router.post('/', [
+    body('name', "Name should be more than 2 characters").isLength({ min: 3 }),
+    body('email', "Enter a valid email").isEmail(),
+    body('password', "Password must be atleast 5 characters").isLength({ min: 5 })
+], (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({ errors: result.array() })
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => res.json({error: "Error occurred check below for more details", message: err.message}));
 })
+
 
 module.exports = router
