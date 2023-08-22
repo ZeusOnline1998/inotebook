861befaad26700e80b45481ba51b4133cc3f57a9
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-08-22 15:23:05 +0530

    Noteitem in card format with defined structure

--- frontend/src/context/notes/NoteState.jsx
+++ frontend/src/context/notes/NoteState.jsx
@@ -22,7 +22,79 @@ const NoteState = (props) => {
             "tag": "personal",
             "date": "2023-08-22T05:29:24.832Z",
             "__v": 0
-        }
+        },
+        {
+            "_id": "64e43b21660b3dacb13c8403",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title",
+            "description": "Description 1",
+            "tag": "General",
+            "date": "2023-08-22T04:35:45.749Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e447b4390526ec27cfc40d",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title 2",
+            "description": "Description 2",
+            "tag": "personal",
+            "date": "2023-08-22T05:29:24.832Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e43b21660b3dacb13c8403",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title",
+            "description": "Description 1",
+            "tag": "General",
+            "date": "2023-08-22T04:35:45.749Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e447b4390526ec27cfc40d",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title 2",
+            "description": "Description 2",
+            "tag": "personal",
+            "date": "2023-08-22T05:29:24.832Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e43b21660b3dacb13c8403",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title",
+            "description": "Description 1",
+            "tag": "General",
+            "date": "2023-08-22T04:35:45.749Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e447b4390526ec27cfc40d",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title 2",
+            "description": "Description 2",
+            "tag": "personal",
+            "date": "2023-08-22T05:29:24.832Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e43b21660b3dacb13c8403",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title",
+            "description": "Description 1",
+            "tag": "General",
+            "date": "2023-08-22T04:35:45.749Z",
+            "__v": 0
+        },
+        {
+            "_id": "64e447b4390526ec27cfc40d",
+            "user": "64e352aad75b0ab15a024039",
+            "title": "My Title 2",
+            "description": "Description 2",
+            "tag": "personal",
+            "date": "2023-08-22T05:29:24.832Z",
+            "__v": 0
+        },
     ]
 
     const [notes, setNotes] = useState(notesInitial);
