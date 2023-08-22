bbde824649d79f8ababbe7a5c6e77a9c64d71abf
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-08-22 14:44:04 +0530

    General structure of home page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,16 +1,9 @@
-import React, { useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/noteContext'
+import React from 'react'
 const About = () => {
-  const a = useContext(NoteContext)
-
-  useEffect(() => {
-    a.update();
-  // eslint-disable-next-line
-  }, []);
 
   return (
     <div>
-      This is About {a.state.name} from {a.state.location}
+      This is About
     </div>
   )
 }
--- frontend/src/context/notes/NoteState.jsx
+++ frontend/src/context/notes/NoteState.jsx
@@ -1,22 +1,10 @@
 import NoteContext from './noteContext';
-import { useState } from 'react';
+
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Zeus",
-        "location": "Mumbai"
-    }
-
-    const [state, setState] = useState(s1);
-
-    const update = () => {
-        setTimeout(() => {
-            setState({"name": "Amit", "location": "Bombay"})
-        }, 2000);
-    }
 
     return (
-        <NoteContext.Provider value={{state, update}} >
+        <NoteContext.Provider value={{}} >
             { props.children }
         </NoteContext.Provider>
     )
