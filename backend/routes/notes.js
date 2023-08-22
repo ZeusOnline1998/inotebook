728093cea57e57cedb4a985da0d73114873a7e1a
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-08-22 10:53:46 +0530

    Update note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,6 +7,7 @@ const { body, validationResult } = require('express-validator');
 
 // Fetch all notes of the logged in user using: GET using: POST "/api/notes/addnote". login required
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    //Fetch all notes wrt to user
     let notes = await Notes.find({user: req.user.id});
     res.json(notes)
 })
@@ -17,18 +18,21 @@ router.post('/addnote', fetchUser, [
     body('description', 'Description should have more than 5 characters').isLength({min: 5}),
 ], async(req, res) => {
     try {
+        //Provide validation for incoming data
         const { title, description, tag } = req.body;
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return res.status(400).json({ errors: result.array() })
         }
 
+        //Create note object using mongoose
         const Note = new Notes({
             title,
             description,
             tag,
             user: req.user.id
         })
+        //Save note and return the saved note
         const savedNote = await Note.save()
         res.json(savedNote)
     } catch (error) {
@@ -37,12 +41,37 @@ router.post('/addnote', fetchUser, [
     }
 })
 
-// Add a new note using: POST "/api/notes/addnote". login required
-router.put('/updatenote/:id', fetchUser, [
-    body('title', 'Title should have more than 3 characters').isLength({ min: 3 }),
-    body('description', 'Description should have more than 5 characters').isLength({ min: 5 }),
-], fetchUser, async (req, res) => {
+
+// Update existing note using: PUT "/api/notes/updatenote/:id". login required
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    //Create newNote object which stored updated data
+    const newNote = {};
+    if(title) {
+        newNote.title = title
+    }
+    if(description) {
+        newNote.description = description
+    }
+    if(tag) {
+        newNote.tag = tag;
+    }
+
+    //Find note which has to be updated
+    const note = await Notes.findById(req.params.id);
+    if(!note) {
+        res.status(404).send("Not Found")
+    }
+    //Allow updation only if user owns this note
+    if(note.user.toString() !== req.user.id){
+        res.status(401).send("Not Allowed")
+    }
+    
+    const updatedNote = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json(updatedNote)
 
 })
+
+
 
 module.exports = router
