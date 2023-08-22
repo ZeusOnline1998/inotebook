c76a48c5d34381560eb22cc1de1c541882587897
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-08-22 13:51:53 +0530

    ContextAPI example

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/noteContext'
 const About = () => {
+  const a = useContext(NoteContext)
+
+  useEffect(() => {
+    a.update();
+  // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
-      This is About
+      This is About {a.state.name} from {a.state.location}
     </div>
   )
 }
